552fb01b13aecd14d44ef6a697ffdf404901775c
a <vca@0x56.net>
2016-04-14 09:51:29 +0200

    small test commit :)

---
+++ git_functions.sh
@@ -0,0 +1,28 @@
+function push(){
+  #git add .
+  git add --all .
+  echo "enter name/message of the commit (or just enter for default message; date user@hostnamense) :"
+  read MSG
+  if [ -z "${MSG}" ]; then
+    echo "empty message, using default"
+		MSG=`whoami`"@"`hostname`" ("`date`")"
+	fi
+	git commit -m "$MSG"
+  git push
+  git lg | head -n20
+}
+
+function pull(){
+  cd .git/ && du -sch && cd .. && du -sch
+  git lg | head -n20
+  #git status
+  git pull
+}
+
+function commit(){
+  #git add .
+  git add --all .
+  MSG=`whoami`"@"`hostname`" ("`date`")"
+  git commit -m $MSG
+}
+

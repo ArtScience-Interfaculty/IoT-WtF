a5d31bcc74f3ada1e0bdd99e1ed827e051c37796
h4nkw1ll1@ms <h4nk@rpj3>
2016-09-25 20:26:03 +0200

    pi@rpj3 (Sun Sep 25 20:26:03 CEST 2016)

--- osc/oscdump.js
+++ osc/oscdump.js
@@ -4,4 +4,5 @@ var portarg = process.argv[2];
 port = 3333;
 if( typeof portarg !== 'undefined' ){ port = portarg } else { console.log("NB. you may use optional port argument") };
 console.log("listening on port: " + port);
+
 var oscServer = new osc.Server( port, '0.0.0.0'); oscServer.on("message", function (msg, rinfo) { console.log(msg); });
--- osc/oscsend.js
+++ osc/oscsend.js
@@ -1,17 +1,14 @@
 #!/usr/bin/node
 var osc = require('node-osc');
-
 var portarg = process.argv[2];
 var tagarg = process.argv[3];
+
 tag = '/wtf';
 port = 3333;
-
 if( typeof portarg !== 'undefined' ){ port = portarg };
 if( typeof tagarg !== 'undefined' ){ tag = tagarg };
 
 console.log('just sent msg: ' + tag + ' to localhost:' + port);
+console.log("you may also do:\n./oscsend.js port /osctag");
 
 var client = new osc.Client('127.0.0.1', port); client.send( tag, Math.random(), function () { client.kill(); });
-
-console.log("you may also do:\n./oscsend.js 9220 /osctag");
-

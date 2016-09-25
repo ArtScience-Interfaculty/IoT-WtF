4cc5eda5016b4ab5eacfa82430e53d99d66dafc7
h4nkw1ll1@ms <h4nk@rpj3>
2016-09-25 20:21:57 +0200

    added port arguments to oscdump.js and oscsend.js examples

--- osc/oscdump.js
+++ osc/oscdump.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
 var osc = require('node-osc');
-var oscServer = new osc.Server(3333, '0.0.0.0'); oscServer.on("message", function (msg, rinfo) { console.log("message:"); console.log(msg); });
+var portarg = process.argv[2];
+port = 3333;
+if( typeof portarg !== 'undefined' ){ port = portarg } else { console.log("NB. you may use optional port argument") };
+console.log("listening on port: " + port);
+var oscServer = new osc.Server( port, '0.0.0.0'); oscServer.on("message", function (msg, rinfo) { console.log(msg); });
--- osc/oscsend.js
+++ osc/oscsend.js
@@ -1,3 +1,17 @@
 #!/usr/bin/node
 var osc = require('node-osc');
-var client = new osc.Client('127.0.0.1', 3333); client.send('/wtf', Math.random(), function () { client.kill(); });
+
+var portarg = process.argv[2];
+var tagarg = process.argv[3];
+tag = '/wtf';
+port = 3333;
+
+if( typeof portarg !== 'undefined' ){ port = portarg };
+if( typeof tagarg !== 'undefined' ){ tag = tagarg };
+
+console.log('just sent msg: ' + tag + ' to localhost:' + port);
+
+var client = new osc.Client('127.0.0.1', port); client.send( tag, Math.random(), function () { client.kill(); });
+
+console.log("you may also do:\n./oscsend.js 9220 /osctag");
+
